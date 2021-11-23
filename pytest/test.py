159acb7b58229e40ea14bf967499fe77e92611cb
lw-hdu <915077145@qq.com>
2021-11-23 15:50:38 +0800

    pytest学习

---
+++ pytest/test.py
@@ -0,0 +1,13 @@
+'''
+Descripttion: 
+version: 
+Author: Liuwen
+Date: 2021-10-11 17:05:45
+LastEditors: sueRimn
+LastEditTime: 2021-11-10 08:48:42
+'''
+import pytest
+class TestClass:
+    def test_one(self):
+        x='this'
+        assert 'h' in x
